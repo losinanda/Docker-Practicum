04cfcf754ff464ef5f9e9b80bef8fae9345a4faa
Nanda <kadekprawira29@gmail.com>
2023-07-06 15:31:39 +0800

    initial commit

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Docker Practicum</title>
+    </head>
+    <body>
+        <h1>Docker Practicum</h1>
+        <p>Name : Kadek Losinanda Prawira.</p>
+        <p>Class : IKI </p>
+        <p>NIM : 2115101046.</p>
+    </body>
+</html>
